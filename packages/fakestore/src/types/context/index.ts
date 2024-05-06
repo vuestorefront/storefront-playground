04d551c313d1d9ba926f778f08b21e9e61bf683a
Wojciech Sikora <wsikora@vuestorefront.io>
2024-05-06 15:30:22 +0200

    remove unnecessary comments

--- packages/fakestore/src/types/context/index.ts
+++ packages/fakestore/src/types/context/index.ts
@@ -4,7 +4,6 @@ import type { MiddlewareConfig, Endpoints } from "../index";
 
 /**
  * Runtime integration context, which includes API client instance, settings, and endpoints that will be passed via middleware server.
- * This interface name is starting with `Boilerplate`, but you should use your integration name in here.
  * */
 export type FakeStoreIntegrationContext = IntegrationContext<
   AxiosInstance,
